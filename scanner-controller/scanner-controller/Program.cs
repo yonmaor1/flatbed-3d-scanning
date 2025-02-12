37b42f8ce50c430bab75af147d680ad9b66b2d8f
Sophia King <seking@andrew.cmu.edu>
2025-02-11 20:05:31 -0500

    Created dotnet projet, added NAPS2 packages, and added sample scanning code to run and test

---
+++ scanner-controller/scanner-controller/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using NAPS2.Images;
+using NAPS2.Images.Gdi;
+using NAPS2.Pdf;
+using NAPS2.Scan;
+
+// Set up the scanning context
+using var scanningContext = new ScanningContext(new GdiImageContext());
+var controller = new ScanController(scanningContext);
+
+// Query for available scanning devices
+var devices = await controller.GetDeviceList();
+
+// Set scanning options
+var options = new ScanOptions
+{
+    Device = devices.First(),
+    PaperSource = PaperSource.Feeder,
+    PageSize = PageSize.A4,
+    Dpi = 300
+};
+
+// Scan and save images
+int i = 1;
+await foreach (var image in controller.Scan(options))
+{
+    image.Save($"page{i++}.jpg");
+}
+
+// Scan and save as PDF
+var images = new List<ProcessedImage>();
+await foreach (var image in controller.Scan(options))
+{
+    images.Add(image);
+}
+var pdfExporter = new PdfExporter(scanningContext);
+await pdfExporter.Export("document.pdf", images);
+
+
+Console.WriteLine("Hello, World!");
